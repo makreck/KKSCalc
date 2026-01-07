d4b617a6c34772eab03a0c6e7d1566cf60f20fe0
makreck <makreck@outlook.de>
2026-01-07 21:31:06 +0100

    Added notes.

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ from modules.times import Times
 
 if __name__ == "__main__":
     Calculator().run()
+
+# Dependencies:
+# pip3 install --upgrade pyperclip Pillow
+
+# Make an executable:
+#  pip install pyinstaller
+#  pyinstaller --hidden-import=PIL._tkinter_finder --onefile --add-data "modules/images/*.png:images" main.py
