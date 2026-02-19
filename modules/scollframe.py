7150421f60c1ee3a9ca95dbe0fa358e07e922950
makreck <makreck@outlook.de>
2026-02-19 11:14:40 +0100

    Improved scrollable frame with mouse wheel support.

--- modules/scollframe.py
+++ modules/scollframe.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import platform
 
 class ScrollableFrame(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
@@ -9,13 +10,14 @@ class ScrollableFrame(tk.Frame):
         self.canvas.configure(yscrollcommand=self.scrollbar.set)
         self.scrollbar.pack(side="right", fill="y")
         self.canvas.pack(side="left", fill="both", expand=True)
-
         self.inner = tk.Frame(self.canvas)
         self.inner_id = self.canvas.create_window((0, 0), window=self.inner, anchor="nw")
 
         self.inner.bind("<Configure>", self.on_frame_configure)
         self.canvas.bind("<Configure>", self.on_canvas_configure)
         self.canvas.bind_all("<MouseWheel>", self.on_mousewheel)
+        self.inner.bind("<Enter>", self.on_bind_wheel_action)
+        self.inner.bind("<Leave>", self.on_unbind_wheel_action)
 
     def on_frame_configure(self, event):
         self.canvas.configure(scrollregion=self.canvas.bbox("all"))
@@ -23,8 +25,27 @@ class ScrollableFrame(tk.Frame):
     def on_canvas_configure(self, event):
         self.canvas.itemconfig(self.inner_id, width=event.width)
 
-    def on_mousewheel(self, event):
-        self.canvas.yview_scroll(int(-1 * (event.delta / 120)), "units")
+    def on_mousewheel(self, event, scroll=None):
+        if platform.system() == "Windows":
+            self.canvas.yview_scroll(int(-event.delta / 120), "units")
+        elif platform.system() == "Linux":
+            self.canvas.yview_scroll(int(scroll), "units")
+        else:
+            self.canvas.yview_scroll(int(-event.delta), "units")
+
+    def on_bind_wheel_action(self, event):
+        if platform.system() == "Linux":
+            self.bind_all("<Button-4>", lambda event: self.on_mousewheel(event, scroll=-1))
+            self.bind_all("<Button-5>", lambda event: self.on_mousewheel(event, scroll=1))
+        else:
+            self.bind_all("<MouseWheel>", self.on_mousewheel)
+
+    def on_unbind_wheel_action(self, event):
+        if platform.system() == "Linux":
+            self.unbind_all("<Button-4>")
+            self.unbind_all("<Button-5>")
+        else:
+            self.unbind_all("<MouseWheel>")
 
     def get(self) -> tk.Widget:
         return self.inner
