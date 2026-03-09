31baaf3454413c2ebff4e1da89881c6cfa5004bd
makreck <makreck@outlook.de>
2026-03-09 10:46:53 +0100

    Path management updated.

--- modules/app_tools.py
+++ modules/app_tools.py
@@ -13,12 +13,25 @@ class AppTools:
             res_path = os.path.join(res_path, folder)
             try:
                 os.makedirs(res_path)
-            except:
+            except FileExistsError:
                 pass
+            except Exception as e:
+                print(e)
         return res_path
     
-    def get_ImageResourcePath(self, image_file = "") -> str:
-        return os.path.join(self.get_ResourcePath("images/"), image_file)
+    def get_ImageResourcePath(self, image_file = "", sub_folder=None) -> str:
+        if sub_folder:
+            path = "images" + sub_folder
+        else:
+            path = "images"
+        return os.path.join(self.get_ResourcePath(path), image_file)
 
-    def get_AppPath(self, folder = None, filename = "") -> str:
-        return os.path.join(AppTools().get_ResourcePath(folder), filename)
+    def get_AppDataPath(self, folder = None, filename = "") -> str:
+        data_path = os.path.dirname(os.path.abspath(__file__))
+        if folder:
+            data_path = os.path.join(data_path, folder)
+            try:
+                os.makedirs(data_path, exist_ok=True)
+            except:
+                pass
+        return os.path.join(data_path, filename)
