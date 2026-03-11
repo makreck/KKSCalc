6b883c911d87d5bc951fb17ce1d7b57186442b10
makreck <makreck@outlook.de>
2026-03-11 10:10:29 +0100

    Imports and dependencies updated.

--- main.py
+++ main.py
@@ -12,17 +12,10 @@ if __name__ == "__main__":
 # Linux:
 #
 #    Clipboard:
-#       sudo apt-get install xclip
+#       sudo apt install -y xclip libjpeg-dev zlib1g-dev libfreetype-dev python3 python3-dev python3-pip python3-tk python3-pyperclip python3-pil python3-cairosvg
 #
-#    Overwrite system wide:
-#       sudo apt remove python3-pil python3-pil.imagetk
-#       sudo apt update
-#       sudo apt install -y libjpeg-dev zlib1g-dev libfreetype6-dev python3 python3-dev python3-pip python3-tk python3-pyperclip python3-pil python3-cairosvg
-#
-#    python3 -m pip install --user --upgrade Pillow --break-system-packages
-#    python3 -m pip install --user --upgrade cairosvg --break-system-packages
-#    python3 -m pip install --user --upgrade tkinterweb --break-system-packages
-#    python3 -m pip install --user --upgrade pyperclip --break-system-packages
+#    Overwrite system wide (possibly dangerous):
+#       python3 -m pip install --user --upgrade Pillow cairosvg tkinterweb pyperclip --break-system-packages
 #
 # Python installer:
 #   pyinstaller --onedir --noconsole --add-data="modules/images/*.png:modules/images" --hidden-import=PIL._tkinter_finder --hidden-import=tkinter main.py
--- modules/__init__.py
+++ modules/__init__.py
@@ -1,24 +1,45 @@
-# import sys, os, math, platform, re, pyperclip, cairosvg, random, json, time
-# import tkinter as tk
+# Dependencies (all):
 
-# from datetime import datetime as dt
-# from tkinterweb import HtmlFrame
-# from pathlib import Path
-# from fractions import Fraction
-# from tkinter import ttk
-# from tkinter.font import Font
-# from tkinter import scrolledtext
-# from functools import partial
-# from enum import Enum
-# from PIL import Image, ImageDraw, ImageTk
-# from io import BytesIO
+import sys, os, math, platform, re, pyperclip, random, json, time, subprocess
+import tkinter as tk
 
-# from modules.config import Config
-# from modules.math_wrapper import MathWrapper        
-# from modules.times import Times
-# from modules.roman_numbers import RomanNumber
-# from modules.thermocouple import Thermocouple
-# from modules.tooltip import ToolTip
-# from modules.scollframe import ScrollableFrame
-# from modules.gui import Gui
-# from modules.calculator import Calculator
+from datetime import datetime as dt
+from tkinterweb import HtmlFrame
+from pathlib import Path
+from fractions import Fraction
+from tkinter import ttk
+from tkinter.font import Font
+from tkinter import scrolledtext
+from itertools import groupby
+from functools import partial
+from enum import Enum
+from PIL import Image, ImageDraw, ImageTk
+from io import BytesIO
+
+from modules.config import Config
+from modules.math_wrapper import MathWrapper        
+from modules.times import Times
+from modules.roman_numbers import RomanNumber
+from modules.thermocouple import Thermocouple
+from modules.tooltip import ToolTip
+from modules.scollframe import ScrollableFrame
+from modules.gui import Gui
+from modules.app_tools import AppTools
+from modules.calculator import Calculator
+from modules.helptext import general_usage
+
+
+# For using cairosvg ...
+if platform.system() == "Windows":
+    # On Windows, cairosvg is not working properly. So, we always need
+    # ready for use PNG images for the buttons!
+    pass
+elif platform.system() == "Linux":
+    # On Linux, cairosvg is available and working properly. So, we can
+    # use the internal SVG resources to build the PNG images we need
+    # for running on the other operating systems.
+    import cairosvg
+else:
+    # On other OS, like MacOS, it is currently unknown if cairosvg library
+    # is available. So, we also use the PNG images only
+    pass
